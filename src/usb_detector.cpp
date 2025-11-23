8db82103fd5b754888a935f64225b13776d0336f
enimaloc <antoine@enimaloc.fr>
2025-11-23 07:24:18 +0100

    fix: define GUID_DEVINTERFACE_VOLUME for MinGW
    Add DEFINE_GUID for GUID_DEVINTERFACE_VOLUME as MinGW does not
    provide this definition in its headers.

--- src/usb_detector.cpp
+++ src/usb_detector.cpp
@@ -8,8 +8,12 @@
 #include <cfgmgr32.h>
 #include <initguid.h>
 #include <usbiodef.h>
-#include <ntddstor.h>
 #include <algorithm>
+
+// Define GUID_DEVINTERFACE_VOLUME if not already defined
+#ifndef GUID_DEVINTERFACE_VOLUME
+DEFINE_GUID(GUID_DEVINTERFACE_VOLUME, 0x53f5630d, 0xb6bf, 0x11d0, 0x94, 0xf2, 0x00, 0xa0, 0xc9, 0x1e, 0xfb, 0x8b);
+#endif
 
 #pragma comment(lib, "setupapi.lib")
 #pragma comment(lib, "cfgmgr32.lib")
